9b0d61292912a950a55a91938112c992e6707acd
João Gilberto <joaogilbertosant@gmail.com>
2024-10-12 16:46:10 -0300

    Fix tratamento de excecoes

--- api/src/main/java/com/matosbrasil/api/handler/ValidacaoExceptionHandler.java
+++ api/src/main/java/com/matosbrasil/api/handler/ValidacaoExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.matosbrasil.api.handler;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.matosbrasil.api.enums.ResponseCode;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class ValidacaoExceptionHandler {
@@ -23,6 +26,12 @@ public class ValidacaoExceptionHandler {
             erros.put(campo, mensagem);
             erros.put("code", ResponseCode.ERROR.getCode());
         });
-        return new ResponseEntity<>(erros, HttpStatus.BAD_REQUEST);
+        
+        // Define o Content-Type como application/json e charset UTF-8
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+        // Retorna a resposta com BAD_REQUEST e os headers com o Content-Type configurado
+        return new ResponseEntity<>(erros, headers, HttpStatus.BAD_REQUEST);
     }
 }
